1affff955c3dce9e0b44a22d29c24f7decd79dce
bagusip <bagusindrt@gmail.com>
2022-06-22 09:50:14 +0700

    remove spring boot test

--- src/test/java/com/example/demo/controller/CustomerControllerTest.java
+++ src/test/java/com/example/demo/controller/CustomerControllerTest.java
@@ -2,7 +2,7 @@ package com.example.demo.controller;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 class CustomerControllerTest {
 
