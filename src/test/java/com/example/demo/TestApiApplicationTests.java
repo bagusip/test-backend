9501b2afbcab6ad0c68689b0e2c270f0427b3858
bagusip <bagusindrt@gmail.com>
2022-06-22 11:15:50 +0700

    test case create user

--- src/test/java/com/example/demo/TestApiApplicationTests.java
+++ src/test/java/com/example/demo/TestApiApplicationTests.java
@@ -1,23 +1,33 @@
 package com.example.demo;
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 //import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.apache.naming.java.javaURLContextFactory;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import com.example.demo.controller.CustomerController;
+import com.example.demo.entity.Customer;
+import com.example.demo.repository.ICustomerRepo;
 
+@SpringBootTest
 public class TestApiApplicationTests {
 
 	@Autowired
-	CustomerController controller = new CustomerController();
-	
+	ICustomerRepo customerRepo;
+
 	@Test
-	void test() {
-		
-		String responseString = controller.hello("World");
-		assertEquals("Hello, Bagus", responseString);
+	void testCreate() {
+		Customer c = new Customer();
+		c.setId(2L);
+		c.setName("tester");
+		c.setAge(4L);
+		c.setLocation("Senayan");
+		customerRepo.save(c);
+		assertNotNull(customerRepo.findById(2L).get());
 	}
 
 
